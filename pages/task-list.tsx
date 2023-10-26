16c820b6fd96542c73f1d82fdde51298d06922a0
Amir Garavand <amirmasoud.g@mtnirancell.ir>
2023-10-26 18:28:23 +0330

    Add route tasklist

---
+++ pages/task-list.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Login from "src/screens/Login";
+import TaskList from "src/screens/TaskList";
+import { api } from "src/services/api";
+import { wrapper } from "src/services/store";
+
+const HomePage: NextPage = () => <TaskList />;
+
+export default HomePage;
+
+export const getServerSideProps = wrapper.getServerSideProps(
+    (store) =>
+        async ({ locale }) => {
+            try {
+                const data = await store
+                    .dispatch(
+                        api.endpoints.getWeatherANSI.initiate({
+                            city: "Tehran",
+                        }),
+                    )
+                    .unwrap();
+            } catch (error) {}
+
+            // you can use:
+            // await Promise.all(
+            //     store.dispatch(api.util.getRunningQueriesThunk()),
+            // );
+            // instead of above await...try...catch
+
+            return {
+                props: {
+                    ...(await serverSideTranslations(locale || "", [
+                        "common",
+                        "home",
+                    ])),
+                },
+            };
+        },
+);
