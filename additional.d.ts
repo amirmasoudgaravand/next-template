c2fb186bd7df490fe488eb570f3c258d4c9acebf
Amir Garavand <amirmasoud.g@mtnirancell.ir>
2023-10-26 12:46:43 +0330

    Replace "primary" color name with "brand"

---
+++ additional.d.ts
@@ -0,0 +1,16 @@
+/**
+ * custom TS type definitions.
+ * here is sample mantine type extension & override. first check theme.ts.
+ */
+// import { MantineThemeOther, Tuple, DefaultMantineColor } from "@mantine/core";
+
+// type ExtendedCustomColors = "brand" | DefaultMantineColor;
+
+// declare module "@mantine/core" {
+//     export interface MantineThemeColorsOverride {
+//         colors: Record<ExtendedCustomColors, Tuple<string, 10>>;
+//     }
+//     export interface MantineThemeOther {
+//         customProperty: string;
+//     }
+// }
