b58a9a88876736cfe03d1594fec2d8a922944b87
Amir Garavand <amirmasoud.g@mtnirancell.ir>
2023-10-26 13:15:49 +0330

    Add state hydrate action to sample slice

---
+++ src/services/slices/shared/reducers.ts
@@ -0,0 +1,22 @@
+import { PayloadAction, Draft } from "@reduxjs/toolkit";
+import { State } from "./state";
+
+export const updateCounter = (
+    state: Draft<State>,
+    action: PayloadAction<State["counter"]>,
+) => {
+    state.counter = action.payload;
+};
+
+export const updateCity = (
+    state: Draft<State>,
+    action: PayloadAction<State["city"]>,
+) => {
+    state.city = action.payload;
+};
+export const updateFoo = (
+    state: Draft<State>,
+    action: PayloadAction<State["foo"]>,
+) => {
+    state.foo = action.payload;
+};
---
+++ src/services/slices/shared/state.ts
@@ -0,0 +1,11 @@
+export interface State {
+    counter: number;
+    city: string;
+    foo: string;
+}
+
+export const initialState: State = {
+    counter: 0,
+    city: "",
+    foo: "",
+};
